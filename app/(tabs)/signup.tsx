c5a3493e2ee702148b3b25dc3619dfdf2481bed3
Alex King <calicoalix@gmail.com>
2024-10-09 20:49:48 +1100

    basic signup functional

--- app/(tabs)/signup.tsx
+++ app/(tabs)/signup.tsx
@@ -1,8 +1,27 @@
-import { SafeAreaView, Text, TextInput, View } from "react-native";
+import {
+  Pressable,
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+} from "react-native";
 import { useState } from "react";
+import axios from "axios";
 
 export default function Signup() {
-  const [text, onChangeText] = useState<string>("");
+  const [username, onChangeUsername] = useState<string>("");
+  const [password, onChangePassword] = useState<string>("");
+  const [email, onChangeEmail] = useState<string>("");
+
+  const onSignupButton = async () => {
+    return await axios
+      .post("http://localhost:8080/signup", { username, password, email })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => console.log(e));
+  };
 
   return (
     <SafeAreaView
@@ -12,11 +31,35 @@ export default function Signup() {
         alignItems: "center",
       }}
     >
+      <Text>Signup</Text>
+
       <TextInput
-        onChangeText={onChangeText}
-        value={text}
+        onChangeText={onChangeUsername}
+        value={username}
         placeholder="username"
       />
+      <TextInput
+        onChangeText={onChangeEmail}
+        value={email}
+        placeholder="email"
+      />
+      <TextInput
+        onChangeText={onChangePassword}
+        value={password}
+        placeholder="password"
+        secureTextEntry={true}
+      />
+
+      <Pressable style={styles.verifyButton} onPress={onSignupButton}>
+        <Text>Verify</Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  verifyButton: {
+    // position: "absolute",
+    // alignSelf: "center",
+    right: 0,
+  },
+});
